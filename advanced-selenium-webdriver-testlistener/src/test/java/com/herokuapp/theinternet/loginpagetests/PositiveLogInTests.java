f0861871cf5cc15d99c6017e2aa4c724dd32b332
User <User@DESKTOP-SV9CFRI>
2023-09-01 13:13:55 +0530

    Initial Commit

---
+++ advanced-selenium-webdriver-testlistener/src/test/java/com/herokuapp/theinternet/loginpagetests/PositiveLogInTests.java
@@ -0,0 +1,49 @@
+package com.herokuapp.theinternet.loginpagetests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.LoginPage;
+import com.herokuapp.theinternet.pages.SecureAreaPage;
+import com.herokuapp.theinternet.pages.WelcomePage;
+
+public class PositiveLogInTests extends TestUtilities {
+
+	@Test
+	public void loginTest() {
+
+		//System.out.println("Starting logIn test");
+		log.info("Starting logIn test");
+		
+		// open main page
+		WelcomePage welcomePage = new WelcomePage(driver, log);
+		welcomePage.openPage();
+		takeScreenshot("WelcomePage opened");
+				
+		// Click on Form Authentication link
+		LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
+		takeScreenshot("LoginPage opened");
+		
+		// enter username and password
+		//execute login
+		SecureAreaPage secureAreaPage = loginPage.logIn("tomsmith", "SuperSecretPassword!");
+		takeScreenshot("SecureAreaPage opened");
+		
+		// verifications
+		// new page url is executed
+		Assert.assertEquals(secureAreaPage.getCurrentUrl(), secureAreaPage.getPageUrl());
+
+		// log out button is visible
+		Assert.assertTrue(secureAreaPage.isLogOutButtonVisible(), "LogOut Button is not visible");
+		
+		// Successful log in message
+		String expectedSuccessMessage = "You logged into a secure area!";
+		String actualSuccessMessage = secureAreaPage.getSuccessMessageText();
+		Assert.assertTrue(actualSuccessMessage.contains(expectedSuccessMessage),
+				"actualSuccessMessage does not contain expectedSuccessMessage\nexpectedSuccessMessage: "
+						+ expectedSuccessMessage + "\nactualSuccessMessage: " + actualSuccessMessage);
+	}
+
+
+}
